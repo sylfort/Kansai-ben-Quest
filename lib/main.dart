125b50c16e4a75b88d7b53fe520108a80010b1ad
sylfort <sylviomafort@gmail.com>
2025-02-28 14:25:43 +0900

    added map borders

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'custom_hit_test_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,122 +8,163 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/hyougo': (context) => const DestinationPage(title: 'Hyougo'),
+        '/kyouto': (context) => const DestinationPage(title: 'Kyouto'),
+        '/mie': (context) => const DestinationPage(title: 'Mie'),
+        '/nara': (context) => const DestinationPage(title: 'Nara'),
+        '/osaka': (context) => const DestinationPage(title: 'Osaka'),
+        '/shiga': (context) => const DestinationPage(title: 'Shiga'),
+        '/wakayama': (context) => const DestinationPage(title: 'Wakayama'),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final String title;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  final List<Map<String, dynamic>> destinations = [
+    {
+      'image': 'assets/images/hyougo.png',
+      'route': '/hyougo',
+      'top': 90.0,
+      'left': 115.0,
+      // Define hit test rectangles (relative to the image's top-left)
+      'hitTestRects': [
+        const Rect.fromLTWH(0, 0, 275, 265), // Adjust these!  Example values.
+        // Add more Rects if hyougo.png has multiple green areas
+      ],
+    },
+    {
+      'image': 'assets/images/wakayama.png',
+      'route': '/wakayama',
+      'top': 420.0,
+      'left': 300.0,
+      'hitTestRects': [
+        const Rect.fromLTWH(0, 30, 180, 215), // Adjust!
+      ],
+    },
+    {
+      'image': 'assets/images/kyouto.png',
+      'route': '/kyouto',
+      'top': 60.0,
+      'left': 260.0,
+      'hitTestRects': [
+        const Rect.fromLTWH(0, 0, 225, 250), // Adjust!
+      ],
+    },
+    //  Mie is commented out in your code.
+    // {
+    //   'image': 'assets/images/mie.png',
+    //   'route': '/mie',
+    //   'top': 280.0,
+    //   'left': 200.0,
+    //   'hitTestRects': [
+    //    const Rect.fromLTWH(0, 0, 200, 150), //Example
+    //   ],
+    // },
+    {
+      'image': 'assets/images/nara.png',
+      'route': '/nara',
+      'top': 325.0,
+      'left': 390.0,
+      'hitTestRects': [
+        const Rect.fromLTWH(71, 0, 150, 220), // Adjust!
+      ],
+    },
+    {
+      'image': 'assets/images/shiga.png',
+      'route': '/shiga',
+      'top': 80.0,
+      'left': 440.0,
+      'hitTestRects': [
+        const Rect.fromLTWH(82, 31, 208, 190), // Adjust!
+      ],
+    },
+    {
+      'image': 'assets/images/osaka.png',
+      'route': '/osaka',
+      'top': 269.0,
+      'left': 342.0,
+      'hitTestRects': [
+        const Rect.fromLTWH(48, 7, 131, 190),
+        const Rect.fromLTWH(0, 179, 114, 216), // Adjust!
+      ],
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: 700,
+            width: 1000,
+            child: ClipRect(
+              child: Stack(
+                children:
+                    destinations.map((destination) {
+                      return Positioned(
+                        top: destination['top'],
+                        left: destination['left'],
+                        child: CustomHitTestImage(
+                          // Use the custom widget
+                          hitTestRects:
+                              destination['hitTestRects']
+                                  .cast<Rect>(), // Cast to List<Rect>
+                          child: InkWell(
+                            //You can remove the onTap here, and the click will be
+                            // handled by CustomHitTestImage
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                destination['route']!,
+                              );
+                            },
+                            child: Image.asset(
+                              destination['image']!,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DestinationPage extends StatelessWidget {
+  const DestinationPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(
-                context,
-              ).textTheme.headlineMedium?.copyWith(color: Colors.blue),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('Welcome to $title!')),
     );
   }
 }
